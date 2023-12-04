7d348e37ce467e13040509dde805116a8dd7e906
sba23352 <sba23352@student.cct.ie>
2023-12-04 18:25:41 +0000

    Initial commit

---
+++ src/minihomework0412/Minihomework0412.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minihomework0412;
+
+/**
+ *
+ * @author amber
+ */
+public class Minihomework0412 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
