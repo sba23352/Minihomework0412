85fff073ffe0b44c649ac885ec3da7cfaa1d658c
sba23352 <sba23352@student.cct.ie>
2023-12-04 18:41:23 +0000

    calculator

--- src/minihomework0412/Minihomework0412.java
+++ src/minihomework0412/Minihomework0412.java
@@ -16,7 +16,7 @@ public class Minihomework0412 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("How are you today?");
-        
+        System.out.println(250 * 20 - 12);
     }
     
 }
