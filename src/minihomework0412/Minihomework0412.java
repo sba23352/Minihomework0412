3551fa585fc480ffba6a7852320ecddd274da8f8
sba23352 <sba23352@student.cct.ie>
2023-12-04 18:40:20 +0000

    how are you

--- src/minihomework0412/Minihomework0412.java
+++ src/minihomework0412/Minihomework0412.java
@@ -15,6 +15,8 @@ public class Minihomework0412 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("How are you today?");
+        
     }
     
 }
